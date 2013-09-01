aa7f201dca12c3d9c622d1414ff0d4695d448612
csweihuang@gmail.com <csweihuang@gmail.com@e039eaa7-eea3-5927-096b-721137851c37>
2013-09-01 20:35:28 +0000

    Update directories

--- inference-framework/checker-framework/checkers/2012-ecoop-eval/run-javad-ot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-benchdir=/cs/huangw5/Projects/proganalysis/projects/benchmarks
-
-time ../binary/javai-ot -d build/ $benchdir/javad/attr/attrFactory.java $benchdir/javad/attr/attrInfo.java $benchdir/javad/attr/codeAttr.java $benchdir/javad/attr/constValueAttr.java $benchdir/javad/attr/deprecAttr.java $benchdir/javad/attr/exceptAttr.java $benchdir/javad/attr/innerClassAttr.java $benchdir/javad/attr/lineNumTabAttr.java $benchdir/javad/attr/localVarTabAttr.java $benchdir/javad/attr/srcFileAttr.java $benchdir/javad/attr/synthAttr.java $benchdir/javad/classfile/classFile.java $benchdir/javad/jconst/constClass_or_String.java $benchdir/javad/jconst/constBase.java $benchdir/javad/jconst/constName_and_Type_info.java $benchdir/javad/jconst/constDouble.java $benchdir/javad/jconst/constFloat.java $benchdir/javad/jconst/constInt.java $benchdir/javad/jconst/constLong.java $benchdir/javad/jconst/constLongConvert.java $benchdir/javad/jconst/constPool.java $benchdir/javad/jconst/constPoolTags.java $benchdir/javad/jconst/constRef.java $benchdir/javad/jconst/constUtf8.java $benchdir/javad/util/access_and_modifier_flags.java $benchdir/javad/util/accData.java $benchdir/javad/util/accString.java $benchdir/javad/util/dataRead.java $benchdir/javad/util/errorMessage.java $benchdir/javad/util/objNameFormat.java $benchdir/javad/util/typeDesc.java $benchdir/javad/javad.java
-
-
