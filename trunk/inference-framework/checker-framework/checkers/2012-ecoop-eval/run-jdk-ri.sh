6f4ef41bab39026e7c3a5d17f1ccf98351cafa11
csweihuang@gmail.com <csweihuang@gmail.com@e039eaa7-eea3-5927-096b-721137851c37>
2013-09-01 20:35:28 +0000

    Update directories

--- trunk/inference-framework/checker-framework/checkers/2012-ecoop-eval/run-jdk-ri.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-benchdir=/cs/huangw5/Projects/proganalysis/projects/benchmarks
-time find $benchdir/jdk-src-1.6/java/ -name '*.java' | grep -v 'awt\|beans' | xargs ../binary/javai-reim -d build/ -cp /cs/huangw5/Softwares/jdk1.6.0_27/jre/lib  
