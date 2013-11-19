b80d4d5f407b2085f022e1ab5f0881f9d87581f7
csweihuang@gmail.com <csweihuang@gmail.com@e039eaa7-eea3-5927-096b-721137851c37>
2013-11-19 19:03:09 +0000

    Add a test case for sflow

--- inference-framework/checker-framework/checkers/inference-tests/sflow/constraintGenerate/ArrayRemoval.java
+++ inference-framework/checker-framework/checkers/inference-tests/sflow/constraintGenerate/ArrayRemoval.java
@@ -1,8 +1,16 @@
 class ArrayRemoval {
+
+    private String[][] visualcalendar = new String[6][7];
+
     void m1() {
         String[] strs = null;
         String[] copy = null;
         copy = strs;
         String s = copy[1];
+    }
+
+
+    void m2() {
+        String s = visualcalendar[1][1];
     }
 }
