fedaf4113d9abbab0456b410857f1f50db653f76
csweihuang <csweihuang@e039eaa7-eea3-5927-096b-721137851c37>
2014-05-08 02:22:38 +0000

    add abstract InferenceTypeVisitor.java

---
+++ trunk/inference-framework/checker-framework/checkers/src/checkers/inference2/InferenceTypeVisitor.java
@@ -0,0 +1,49 @@
+package checkers.inference2;
+
+import checkers.basetype.BaseTypeVisitor;
+import checkers.types.AnnotatedTypeMirror.AnnotatedArrayType;
+import checkers.types.AnnotatedTypeMirror.AnnotatedDeclaredType;
+import checkers.types.AnnotatedTypeMirror.AnnotatedPrimitiveType;
+import checkers.types.AnnotatedTypeMirror.AnnotatedTypeVariable;
+import checkers.types.AnnotatedTypeMirror.AnnotatedWildcardType;
+
+import com.sun.source.tree.CompilationUnitTree;
+import com.sun.source.tree.Tree;
+
+public abstract class InferenceTypeVisitor<Checker extends InferenceChecker>
+		extends BaseTypeVisitor<InferenceChecker> {
+
+	public InferenceTypeVisitor(InferenceChecker checker,
+			CompilationUnitTree root) {
+		super(checker, root);
+	}
+
+	@Override
+	protected TypeValidator createTypeValidator() {
+		return new InferenceTypeValidator();
+	}
+
+	protected class InferenceTypeValidator extends TypeValidator {
+		@Override
+        public Void visitDeclared(AnnotatedDeclaredType type, Tree tree) {
+			return null;
+		}	
+        @Override
+        public Void visitPrimitive(AnnotatedPrimitiveType type, Tree tree) {
+        	return null;
+        }
+        @Override
+        public Void visitArray(AnnotatedArrayType type, Tree tree) {
+        	return null;
+        }
+        @Override
+        public Void visitTypeVariable(AnnotatedTypeVariable type, Tree tree) {
+        	return null;
+        }
+        @Override
+        public Void visitWildcard(AnnotatedWildcardType type, Tree tree) {
+        	return null;
+        }
+	}
+	
+}
