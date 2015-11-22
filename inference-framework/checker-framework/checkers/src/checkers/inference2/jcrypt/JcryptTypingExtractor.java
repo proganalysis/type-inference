6a014120083b07d99b6a2ec18ace833e60cd9fa4
Yao Dong <dongy6@rpi.edu>
2015-11-22 01:46:23 -0600

    Choose the type for callsite qualifier.

--- inference-framework/checker-framework/checkers/src/checkers/inference2/jcrypt/JcryptTypingExtractor.java
+++ inference-framework/checker-framework/checkers/src/checkers/inference2/jcrypt/JcryptTypingExtractor.java
@@ -1,23 +1,28 @@
-/**
- * 
- */
 package checkers.inference2.jcrypt;
 
 import java.util.List;
+import java.util.Set;
+
+import javax.lang.model.element.AnnotationMirror;
 
 import checkers.inference2.Constraint;
 import checkers.inference2.InferenceChecker;
 import checkers.inference2.MaximalTypingExtractor;
+import checkers.inference2.Reference;
+import checkers.inference2.Reference.MethodAdaptReference;
+import checkers.util.AnnotationUtils;
 
 /**
- * @author huangw5
+ * @author dongy6
  *
  */
 public class JcryptTypingExtractor extends MaximalTypingExtractor {
+	
+	private JcryptChecker checker;
 
 	public JcryptTypingExtractor(InferenceChecker c) {
 		super(c);
-		// TODO Auto-generated constructor stub
+		checker = (JcryptChecker) c;
 	}
 
 	/* (non-Javadoc)
@@ -25,8 +30,16 @@ public class JcryptTypingExtractor extends MaximalTypingExtractor {
 	 */
 	@Override
 	public List<Constraint> extract() {
-		// TODO Auto-generated method stub
-		return super.extract();
+		List<Constraint> conflicts = super.extract();
+		for (Constraint c : conflicts) {
+			MethodAdaptReference adaptRef = (MethodAdaptReference) c.getRight();
+			Reference callsiteRef = adaptRef.getContextRef();
+			// update the annotation for the callsite reference
+			Set<AnnotationMirror> finalAnnos = AnnotationUtils.createAnnotationSet();
+			finalAnnos.add(checker.SENSITIVE);
+			callsiteRef.setAnnotations(finalAnnos, checker);
+		}
+		return typeCheck();
 	}
 
 }
